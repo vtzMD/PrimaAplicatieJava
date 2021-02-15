0d25b9bd51b551eb3aa822b1742c78c757311d35
Vlad <matei.vlad18@yahoo.com>
2021-02-15 21:09:12 +0200

    Upload Cod PrimaAplicatieJava

---
+++ src/ro/java/ctrln/Main.java
@@ -0,0 +1,10 @@
+package ro.java.ctrln;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //JAR - Java Archive
+        //JAVA_HOME
+	    System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
+    }
+}
