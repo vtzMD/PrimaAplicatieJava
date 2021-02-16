9cb52b51dd4e5d18664b143ecf59dc0850a5b470
Vlad <matei.vlad18@yahoo.com>
2021-02-16 21:27:40 +0200

    Modificare Main.java - stergere comentariu

--- src/ro/java/ctrln/Main.java
+++ src/ro/java/ctrln/Main.java
@@ -3,8 +3,6 @@ package ro.java.ctrln;
 public class Main {
 
     public static void main(String[] args) {
-
-        //JAVA_HOME
 	    System.out.println("Invat Limbajul Java si despre masina virtuala Java!");
     }
 }
